9c26e2072ba22b64dbe58974815ea3ddde2a45fb
thdin <thdin@hugetech.com.cn>
2021-02-26 18:26:01 +0800

    up

--- VSCode/1.java
+++ VSCode/1.java
@@ -1,5 +1,5 @@
 class q {
     public static void main(String[] args) {
-        System.out.println(System.getProperty("java.version"));
+        System.out.println("Java版本:"+System.getProperty("java.version"));
     }
 }

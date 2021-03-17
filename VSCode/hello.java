194aea23dbf6446d5a176bce20c261c4fa1bfda1
thdin <444543565@qq.com>
2021-03-17 18:28:18 +0800

    up

--- VSCode/hello.java
+++ VSCode/hello.java
@@ -1,5 +1,9 @@
-class q {
+class hello {
     public static void main(String[] args) {
         System.out.println(System.getProperty("java.version"));
+        assert true : "true!";
+        System.out.println("true");
+        // assert false : "false!";
+        // System.out.println("false");
     }
 }
